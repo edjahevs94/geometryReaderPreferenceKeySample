885d0dbb322821d8831fb956fa871f896a8c975f
Edgard <20133252@ealoe.ulima.edu.pe>
2023-04-03 16:07:00 -0500

    Initial Commit

---
+++ SpotifyEffect/SpotifyEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyEffectApp.swift
+//  SpotifyEffect
+//
+//  Created by EdgardVS on 3/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
